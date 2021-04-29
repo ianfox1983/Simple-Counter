34f52ce6fb69006bffe947558ea6cdd69e564282
Mario Sorrentino <eclest@gmail.com>
2021-04-29 15:11:05 +0000

    Primer intento

---
+++ src/js/component/iconclock.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function IconClock() {
+	return (
+		<div className="card">
+			<img
+				className="card-img-top img-fluid img-thumbnail"
+				src="https://media.istockphoto.com/photos/bomb-with-clock-timer-3d-countdown-picture-id461877887?k=6&m=461877887&s=170667a&w=0&h=uZBhOosguOiXT2Cdtv2VsaKxJAM8l2DlEwuVEfZjaKo="
+				alt="Card image cap"
+			/>
+			<div className="card-body">
+				<h5 className="card-title text-center">Awesome Counter</h5>
+				<p className="card-text text-center">
+					Hola este es un texto super para que leas lo cool que es
+					nuestro Counter :D
+				</p>
+
+				<a href="#" className="btn btn-dark">
+					Click to Start
+				</a>
+			</div>
+		</div>
+	);
+}
